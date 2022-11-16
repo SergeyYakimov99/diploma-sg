2efff0bd6867021d33a2f9b24de598b4cb4dc8b2
SergeyYakimov99 <yakim-sg@ya.ru>
2022-11-16 21:10:00 +0500

    create model User

--- todolist/core/models.py
+++ todolist/core/models.py
@@ -1,3 +1,8 @@
-from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    pass
+    # эта константа содержит список с полями,
+    # которые необходимо заполнить при создании пользователя
+ #   REQUIRED_FIELDS = ['first_name', 'last_name', 'email']
