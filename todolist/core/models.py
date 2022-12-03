cabcb9e03164a785e9aae99cf63c7b1ac59fbab3
SergeyYakimov99 <yakim-sg@ya.ru>
2022-12-03 16:07:03 +0500

    исправили settings

--- todolist/core/models.py
+++ todolist/core/models.py
@@ -2,7 +2,6 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
     # эта константа содержит список с полями,
     # которые необходимо заполнить при создании пользователя
     REQUIRED_FIELDS = ['first_name', 'last_name', 'email']
